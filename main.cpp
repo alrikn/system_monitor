43e2cd3398f6aad81423811f121161aed5af57cc
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-01-31 10:28:40 +0100

    feat: added cpumodule

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unistd.h>  // For sleep
 
+#include "CpuModule.hpp"
 #include "Imodule.hpp"
 #include "TimeModule.hpp"
 #include "BasicNcurses.hpp"
@@ -14,8 +15,10 @@ also the quit is broken, and i can't be bothered to figure out why
 void diplay()
 {
     //we make all the modules taht we are going to use in our displays
+    //everything is a shered pointer in case we need to pass stuff to a 
     std::vector<std::shared_ptr<Imodule>> modules;
     modules.push_back(std::make_shared<TimeModule>());
+    modules.push_back(std::make_shared<CpuModule>());
 
     //make the display (rn is the basic one, but we'll have to add custom stuff later on)
     BasicNcurses display;
