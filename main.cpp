601084cbfd4f3b4c2d9961a72a1218e48f425aab
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-01-31 18:07:05 +0100

    feat: extra modules

--- main.cpp
+++ main.cpp
@@ -61,6 +61,9 @@ int main(int argc, char **argv)
     modules.push_back(std::make_shared<TimeModule>());
     modules.push_back(std::make_shared<CpuNumModule>());
     modules.push_back(std::make_shared<BatteryModule>());
+    modules.push_back(std::make_shared<CpuUseModule>());
+    modules.push_back(std::make_shared<CpuNameModule>());
+    modules.push_back(std::make_shared<BatteryModule>());
 
     // create and run nCurses display
     IDisplay *display = argv_reader(argc, argv);
