86b0c73e628b9009425a353c494aac8ca007dfd5
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-01-31 16:07:15 +0100

    feat: bat percentage

---
+++ BatteryModule.hpp
@@ -0,0 +1,33 @@
+
+#ifndef INCLUDED_BATTERYMODULE_HPP
+    #define INCLUDED_BATTERYMODULE_HPP
+
+#include <iostream>
+#include <string>
+#include "IModule.hpp"
+#include <fstream>
+
+class BatteryModule : public Krell::IModule<float> { //we have to declare what the type T is for the get_value
+    private:
+        float _bat = 1;
+    public:
+        void update() override {
+            std::ifstream batt_file;
+
+            batt_file.open("/sys/class/power_supply/BAT0/capacity");
+
+            if (batt_file.fail())
+                return;
+            batt_file >> _bat;
+            _bat /= 100;
+        }
+        std::string get_name() override {return "Battery Percentage";}
+        int get_height() override {return 1;}
+
+        float get_value() override {return _bat;}
+        std::string get_string() override {return std::to_string(_bat);}
+        bool is_percentage() override {return true;}
+};
+
+
+#endif
