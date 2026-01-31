692d30ad1292e07eee45d185bae102a77978b688
Sofia Rodriguez <sofia-isabel.rodriguez-cura@epitech.eu>
2026-01-31 14:40:06 +0100

    ADD: SFML FIRST VER

---
+++ SFMLWindow.hpp
@@ -0,0 +1,56 @@
+#ifndef SFMLWINDOW_HPP
+    #define SFMLWINDOW_HPP
+
+#include "IDisplay.hpp"
+#include <SFML/Graphics.hpp>
+#include <memory>
+#include <vector>
+#include <string>
+
+namespace Krell {
+
+class SFMLWindow : public IDisplay::Window
+{
+    private:
+        sf::RenderWindow _window;
+        sf::Font _font;
+        const int _width = 500;
+        int _totalHeight;
+        std::string _title;
+
+        const sf::Color _bgColor = sf::Color(30,30,30);
+        const sf::Color _textColor = sf::Color(45,45,45);
+        const sf::Color _moduleBgColor = sf::COlor(220,220,220);
+        const sf::Color _titleColor = sf::Color(255,155,0);
+        const sf::Color _percentageColor = sf::Color(100,200,255)
+
+        //helpers
+
+        void drawModule(const std::shared_ptr<IModuleBase>& module, int yOffset);
+
+        void drawPercentageBar(const std::shared_ptr<IModuleBase>& module, int yPos, int width);
+
+        float parsePercentage(const std::string& value) const;
+
+        sf::Color getProgressBarColor(float percentage) const;
+
+    public:
+        SFMLWindow();
+
+        void create() override;
+        void destroy() override;
+
+        void calcHeight(const std::vector<std::shared_ptr<IModuleBase>>& modules);
+
+        void render(const std::vector<std::shared_ptr<IModuleBase>>& modules);
+
+        bool isOpen() const;
+
+        void handleEvents();
+}
+
+}
+
+
+
+#endif
