bd7be806334d9d6d88a107e71ce7dc98d6da4584
Alrik Neihouser <alrik.neihouser@epitech.eu>
2026-01-31 14:17:37 +0100

    feat: cpu percentage

--- CpuModule.hpp
+++ CpuModule.hpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <iomanip>
 
 class CpuNumModule : public Krell::IModule<int> { //we have to declare what the type T is for the get_value
     private:
@@ -22,15 +24,84 @@ class CpuNumModule : public Krell::IModule<int> { //we have to declare what the 
                         break;
                     _num += 1;
                 }
+                _initialised = true;
             }
         }
 
-        std::string get_name() override { return "Time"; }
+        std::string get_name() override { return "Cpu Number"; }
         int get_height() override { return 1; }
 
         int get_value() override {return _num;}
         std::string get_string() override {return std::to_string(_num);}
         bool is_percentage() override {return false;}
+
+};
+
+class CpuUseModule : public Krell::IModule<float> { //we have to declare what the type T is for the get_value
+    private:
+        int _num = 0;
+        float _use_percentage = 0;
+        bool _initialised = false;
+        struct CpuTimes {
+            long idle;
+            long total;
+        };
+        std::vector<CpuTimes> _previous;
+        float _usage = 0;
+
+
+        CpuTimes readLine(const std::string& line) const
+        {
+            std::istringstream iss(line);
+            std::string cpu;
+            long user, nice, system, idle, iowait, irq, softirq, steal; //no coding style muahhaha
+
+            iss >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+
+            CpuTimes t;
+            t.idle = idle + iowait;
+            t.total = user + nice + system + idle + iowait + irq + softirq + steal;
+            return t;
+        }
+    public:
+        //https://stackoverflow.com/questions/3017162/how-to-get-total-cpu-usage-in-linux-using-c
+        void update() override {
+            std::ifstream file("/proc/stat");
+            std::string line;
+            std::vector<CpuTimes> current;
+            _usage = 0;
+
+            while (std::getline(file, line)) {
+                if (line.rfind("cpu", 0) != 0)
+                    break;
+                current.push_back(readLine(line));
+                _num += 1;
+            }
+
+            if (!_initialised) {
+                _previous = current;
+                _initialised = true;
+                return;
+            }
+            long idle_delta = 0;
+            long total_delta = 0;
+            for (std::size_t i = 0; i < current.size(); i++) {
+                idle_delta += current[i].idle - _previous[i].idle;
+                total_delta += current[i].total - _previous[i].total;
+                if (total_delta > 0) {
+                    _usage += (1.f - static_cast<float>(idle_delta) / total_delta) * 100.f;
+                }
+            }
+            _usage /= _num;
+
+        }
+
+        std::string get_name() override { return "Cpu %"; }
+        int get_height() override { return 1; }
+
+        float get_value() override {return _usage;}
+        std::string get_string() override {return std::to_string(_usage);}
+        bool is_percentage() override {return true;}
 
 };
 
